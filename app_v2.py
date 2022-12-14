631985a24cc637c6cb009e6bf74502eda5b95bb5
Evangelia ZVE <evangelia.grigoria.zve@gmail.com>
2022-12-14 12:52:36 +0100

    Update app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -1,19 +1,24 @@
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 
-# Use the AutoTokenizer and AutoModelForQuestionAnswering classes
-# to load the BERT tokenizer and model
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-model = AutoModelForQuestionAnswering.from_pretrained("bert-base-uncased")
 
-# Create a text input field using Streamlit
-text_input = st.text_input("Enter a query:")
+def main():
+    # Use the AutoTokenizer and AutoModelForQuestionAnswering classes
+    # to load the BERT tokenizer and model
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+    model = AutoModelForQuestionAnswering.from_pretrained("bert-base-uncased")
 
-# Use the BERT tokenizer to tokenize the input text
-tokens = tokenizer.tokenize(text_input)
+    # Create a text input field using Streamlit
+    text_input = st.text_input("Enter a query:")
 
-# Use the BERT model to predict the most likely completion for the input text
-prediction = model.predict(tokens)
+    # Use the BERT tokenizer to tokenize the input text
+    tokens = tokenizer.tokenize(text_input)
 
-# Display the predicted completion in a Streamlit output field
-st.write("Predicted completion:", prediction)
+    # Use the BERT model to predict the most likely completion for the input text
+    prediction = model.predict(tokens)
+
+    # Display the predicted completion in a Streamlit output field
+    st.write("Predicted completion:", prediction)
+    
+if __name__ == '__main__':
+    main()
