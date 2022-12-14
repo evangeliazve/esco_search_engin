b617c6318705689449f232a776061eb930e22ae0
Evangelia ZVE <evangelia.grigoria.zve@gmail.com>
2022-12-14 14:02:36 +0100

    Update app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -15,14 +15,13 @@ def main():
 
     # Use the `await st.compute` method to run the BERT model in the background
     # and generate autocomplete suggestions
-    async def suggestions():
-              await st.compute(
-                         generate_suggestions,
-                         text_input=text_input,
-                         model=model,
-                         tokenizer=tokenizer
-                           )
-    suggestions = suggestions()
+    suggestions = await st.compute(
+        generate_suggestions,
+        text_input=text_input,
+        model=model,
+        tokenizer=tokenizer
+    )
+
     # Create a dropdown menu of suggestions using the `st.selectbox` method
     selected_suggestion = st.selectbox(
         "Select a suggestion:",
