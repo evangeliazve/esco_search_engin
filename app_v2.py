fc03a3b310e4027a809fdcba3905df79327d5596
Evangelia ZVE <evangelia.grigoria.zve@gmail.com>
2022-12-14 13:17:26 +0100

    Update app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -3,22 +3,28 @@ from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 
 
 def main():
-    # Use the AutoTokenizer and AutoModelForQuestionAnswering classes
-    # to load the BERT tokenizer and model
-    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-    model = AutoModelForQuestionAnswering.from_pretrained("bert-base-uncased")
+    import streamlit as st
+    from transformers import AutoModelWithLMHead, AutoTokenizer
 
-    # Create a text input field using Streamlit
-    text_input = st.text_input("Enter a query:")
+    # Load the BERT model and tokenizer
+    model = AutoModelWithLMHead.from_pretrained("bert-base-cased")
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
 
-    # Use the BERT tokenizer to tokenize the input text
-    tokens = tokenizer.tokenize(text_input)
+    # Create a text input field
+    text_input = st.text_input("Enter some text:", key="text_input")
 
-    # Use the BERT model to predict the most likely completion for the input text
-    prediction = model.predict(tokens)
+    # Use the `await st.compute` method to run the BERT model in the background
+    # and generate autocomplete suggestions
+    autocomplete_results = await st.compute(
+        generate_autocomplete_suggestions,
+        text_input=text_input,
+        model=model,
+        tokenizer=tokenizer
+    )
 
-    # Display the predicted completion in a Streamlit output field
-    st.write("Predicted completion:", prediction)
+    # Display the autocomplete suggestions
+    for result in autocomplete_results:
+        st.write(result)
     
 if __name__ == '__main__':
     main()
