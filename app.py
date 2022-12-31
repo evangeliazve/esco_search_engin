b60334882eecb199ef8894a6a6dedf19c5024754
Evangelia ZVE <evangelia.grigoria.zve@gmail.com>
2022-12-31 11:26:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -60,10 +60,8 @@ st.title("Moteur de recherche fonctions")
 # User search
 user_input = st.text_input("Search by query")
 
-while True:
-    encoded_user_input = vector_search([user_input], model, faiss_index, num_results)
-    data = pd.DataFrame(data)
-    data["id"] = data.index
-    frame = data[data.id.isin(encoded_user_input)]    
-    st.write(frame)
-    time.sleep(5)
+encoded_user_input = vector_search([user_input], model, faiss_index, num_results)
+data = pd.DataFrame(data)
+data["id"] = data.index
+frame = data[data.id.isin(encoded_user_input)]    
+st.write(frame)
