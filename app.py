42250a6dacd4238318a152098e10d499b02ccf34
Evangelia ZVE <evangelia.grigoria.zve@gmail.com>
2022-12-31 11:52:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -47,8 +47,8 @@ def vector_search(query, model, index, num_results=10):
     
     """
     vector = model.encode(list(query))
-    D, I = index.search(np.array(vector).astype("float32"), k=20)
-    return [j for i,j in enumerate(I[0]) if D[0][i] > 0.5]
+    D, I = index.search(np.array(vector).astype("float32"), k=5)
+    return [j for i,j in enumerate(I[0]) if D[0][i] < 0.1]
 
 def main():
     data = read_data()
