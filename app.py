b04074949df74c267e569658b645ae0b53f74173
Evangelia ZVE <evangelia.grigoria.zve@gmail.com>
2022-12-31 10:10:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,6 @@ import pandas as pd
 from transformers import AutoModelWithLMHead, AutoTokenizer
 from scipy.spatial import distance
 
-model = AutoModelWithLMHead.from_pretrained("bert-base-cased")
-tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
-
 st.title("Autosuggestion App")
 
 data = ["apple", "banana", "orange", "grape", "strawberry"]
@@ -22,10 +19,20 @@ suggestions = []
 
 if len(input_text) > 1:
   input_tokens = tokenizer.encode(input_text, return_tensors="pt").to("cpu")
-  output = model.generate(input_tokens, max_length=20, top_p=0.9, top_k=10, temperature=0.8)
-  output_text = tokenizer.decode(output[0], skip_special_tokens=True)
-  suggestions = output_text.split(" ")
-  suggestions = [s for s in suggestions if s in data]
+  input_vec = model(input_tokens)[0][0][0]
+  suggestions = []
+  for item in data:
+    item_tokens = tokenizer.encode(item, return_tensors="pt").to("cpu")
+    item_vec = model(item_tokens)[0][0][0]
+    sim = distance.cosine(input_vec, item_vec)
+    if sim < 0.5:
+      suggestions.append(item)
   suggestions = sorted(suggestions, key=lambda x: distance.cosine(input_tokens, tokenizer.encode(x)))
 
 st.write("Suggestions:", suggestions)
+
+custom_text = st.text_input("Enter your own text")
+if custom_text != "":
+  st.write("You entered:", custom_text)
+
+st.run()
