4174e097c39deb821bb5e86713736ef6f7a436a8
Evangelia ZVE <evangelia.grigoria.zve@gmail.com>
2022-12-31 10:38:47 +0100

    Update app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -2,30 +2,55 @@ import streamlit as st
 import asyncio
 from transformers import AutoTokenizer, AutoModelWithLMHead
 
+import streamlit as st
+import faiss
+import torch
+from transformers import BertTokenizer, BertModel
 
-async def main():
-    
-    # Load the BERT model and tokenizer
-    model = AutoModelWithLMHead.from_pretrained("bert-base-cased")
-    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+# Load the BERT model and tokenizer
+model = BertModel.from_pretrained('bert-base-uncased')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-    # Create a text input field
-    text_input = st.text_input("Enter some text:")
+# Define your predefined list of possible results
+results = [
+    'apple',
+    'banana',
+    'orange',
+    'grapes',
+    'strawberry',
+    'kiwi',
+    'mango',
+    'pear',
+    'watermelon'
+]
 
-    # Use the `await st.compute` method to run the BERT model in the background
-    # and generate autocomplete suggestions
-    suggestions = await st.compute(
-        generate_suggestions,
-        text_input=text_input,
-        model=model,
-        tokenizer=tokenizer
-    )
+# Tokenize and encode the results using BERT
+encoded_results = []
+for result in results:
+    input_ids = torch.tensor([tokenizer.encode(result, add_special_tokens=True)])
+    with torch.no_grad():
+        encoded_results.append(model(input_ids)[0][0][0].numpy())
 
-    # Create a dropdown menu of suggestions using the `st.selectbox` method
-    selected_suggestion = st.selectbox(
-        "Select a suggestion:",
-        suggestions,
-        index=0
-    )
+# Create a Faiss index from the encoded results
+index = faiss.IndexFlatL2(768)
+index.add(encoded_results)
 
-asyncio.run(main())
+# Create a text input field in the Streamlit app
+query = st.text_input('Enter a search query:')
+
+# Use Faiss to retrieve the most similar results to the query
+@st.cache
+def get_suggestions(query):
+    input_ids = torch.tensor([tokenizer.encode(query, add_special_tokens=True)])
+    with torch.no_grad():
+        encoded_query = model(input_ids)[0][0][0].numpy()
+    distances, indices = index.search(encoded_query.reshape(1, -1), k=5)
+    return [results[i] for i in indices[0]]
+
+if len(query) > 1:
+    suggestions = get_suggestions(query)
+
+    # Display the list of suggestions to the user
+    st.write('Suggestions:')
+    for suggestion in suggestions:
+        st.write(suggestion)
